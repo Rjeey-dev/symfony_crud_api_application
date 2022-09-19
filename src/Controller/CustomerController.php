93ec8fc88914cfdcdfabeaa544468fd1cffeea88
Rjeey-dev <valera.abashyn@gmail.com>
2022-09-19 23:10:48 +0300

    fix_error

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Form\Type\CustomerType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 
-class CustomerController extends AbstractController
+class CustomerController extends AbstractApiController
 {
-
-    public function __construct(private ManagerRegistry $doctrine) {
-
-    }
+    public function __construct(private ManagerRegistry $doctrine) {}
 
     public function indexAction(Request $request): Response
     {
@@ -23,8 +21,23 @@ class CustomerController extends AbstractController
         return $this->json($customers);
     }
 
-    public function createAction(Request $request)
+    public function createAction(ManagerRegistry $doctrine,Request $request)
     {
+        $form = $this->buildForm(CustomerType::class);
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted()) {
+            // throw exception
+            print 'Error';
+            exit();
+        }
+
+        /** @var Customer $customer*/
+        $customer = $form->getData();
+
+        $this->doctrine->getManager()->persist($customer);
+        $this->doctrine->getManager()->flush();
+
         return $this->json('');
     }
 }
