2e085efc2c8d9fef70edd5fd9ca06e683143d57f
Rjeey-dev <valera.abashyn@gmail.com>
2022-09-27 10:33:57 +0300

    created_crud_proudct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Customer;
-use App\Form\Type\CustomerType;
+use App\Entity\Product;
+use App\Form\Type\ProductType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\Persistence\ManagerRegistry;
 
-class CustomerController extends AbstractApiController
+class ProductController extends AbstractApiController
 {
     public function __construct(private ManagerRegistry $doctrine) {}
 
     public function indexAction(Request $request): Response
     {
-        $customers = $this->doctrine->getRepository(Customer::class)->findAll();
-        return $this->json($customers);
+        $products = $this->doctrine->getRepository(Product::class)->findAll();
+        return $this->respond($products);
     }
 
     public function createAction(Request $request): Response
     {
-        $form = $this->buildForm(CustomerType::class);
+        $form = $this->buildForm(ProductType::class);
         $form->handleRequest($request);
 
         if (!$form->isSubmitted() || !$form->isValid()) {
             return $this->respond($form, Response::HTTP_BAD_REQUEST);
         }
 
-        /** @var Customer $customer*/
-        $customer = $form->getData();
-        $this->doctrine->getManager()->persist($customer);
+        /** @var Product $product*/
+        $product = $form->getData();
+        $this->doctrine->getManager()->persist($product);
         $this->doctrine->getManager()->flush();
-
-        return $this->respond($customer);
+        return $this->respond($product);
     }
 }
